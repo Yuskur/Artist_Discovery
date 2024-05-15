554345686eabf6e3e58858a2647c24999ded51f5
yisakor <yisakor.melke@gmail.com>
2024-05-15 16:58:20 -0400

    Login

--- front_end/src/paths/Login.jsx
+++ front_end/src/paths/Login.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import './Login.css';
 
 function Login(){
     return(
-        <div> 
-            This is the Login page
+        <div className="Login"> 
+            <form action="">
+                <h1 className="Login-title">Login</h1>
+                <hr className="firstLine" />
+                <div className="Username">
+                    <input className="Username-textbox" 
+                    type="text" placeholder="Username" required/>
+                </div>
+                <div className="Password">
+                    <input className="Password-textbox" 
+                    type="text" placeholder="Password" required/>
+                </div>
+                <div className="login">
+                <button className="Login-button"
+                    onClick={console.log("logging in...")}>
+                        login
+                </button>
+            </div>
+            </form>
+            <h4 class="or">or</h4>
+            <div className="signup">
+                <button className="Signup-button"
+                    onClick={console.log("signing up...")}>
+                        Signup
+                </button>
+            </div>
         </div>
     );
 }
